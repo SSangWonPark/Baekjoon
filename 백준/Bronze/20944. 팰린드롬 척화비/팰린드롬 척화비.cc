28b566b3c8e979bf955fba2bd6df0c6dc6fccc29
SangWon Park <113331695+SSangWonPark@users.noreply.github.com>
2025-04-03 13:02:02 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("a");
+    }
+
+    return 0;
+}
+
+// 모두 같은 문자이면 무조건 팰린드롬 + 수미상관
