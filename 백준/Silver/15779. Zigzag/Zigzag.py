fad4ea84476cc1c0c9006eb1c6ee578528f60682
SangWon Park <113331695+SSangWonPark@users.noreply.github.com>
2025-06-28 22:45:44 +0900

    [Silver V] Title: Zigzag, Time: 52 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15779. Zigzag/Zigzag.py
+++ 백준/Silver/15779. Zigzag/Zigzag.py
@@ -1,44 +1,29 @@
-n = int (input())
-num = list(map(int, input().split()))
+n = int(input())
+a = list(map(int, input().split()))
 
-upcount = 0
-downcount = 0
-count = 1
-ans = 1
+if n < 3:
+    print(n)
+    exit()
 
-for i in range(0, n - 1):
-  
-  if num[i] < num[i + 1]:
-    
-    if upcount == 0: 
-      upcount = 1
-      count += 1
-      downcount = 0
+ans = 2
+tmp = 2
+
+for i in range(n - 2):
+    x, y, z = a[i], a[i + 1], a[i + 2]
+
+    if (x <= y <= z) or (x >= y >= z):
+        tmp = 2
     else:
-      upcount = 1
-      count = 2
-      downcount = 0
+        tmp += 1
 
-  elif num[i] > num[i + 1]:
-    
-    if downcount == 0:
-      downcount = 1
-      count += 1
-      upcount = 0
-    else:
-      downcount = 1
-      count = 2
-      upcount = 0
-
-  else:
-    count = 2
-    upcount = 0
-    downcount = 0
-  
-  if ans < count:
-    ans = count
+    ans = max(ans, tmp)
 
 print(ans)
 
 # 한글자가 들어오는 경우도 생각. ans = 1
 # 1 1 1 이 들어온 경우, 2개 출력되어야 함.
+# 부분 성공: 33점
+
+# 뭐가 문제일까
+# 1 3 5 2 이면 2가 아니라 3이 나와야 하나? 이것도 아님.
+# 3개씩 보면서 갈까. 1개면 무조건 1, 2개면 무조건 2, 3개인 경우만 처리
