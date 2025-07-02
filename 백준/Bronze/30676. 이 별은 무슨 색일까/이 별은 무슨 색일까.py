b34bcdc1785136196ead5d88cbcc08ab10fb4eed
SangWon Park <113331695+SSangWonPark@users.noreply.github.com>
2025-07-02 21:46:10 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num >= 620 and num <= 780:
+  print("Red")
+elif num >= 590 and num < 620:
+  print("Orange")
+elif num >= 570 and num < 590:
+  print("Yellow")
+elif num >= 495 and num < 570:
+  print("Green")
+elif num >= 450 and num < 495:
+  print("Blue")
+elif num >= 425 and num < 450:
+  print("Indigo")
+elif num >= 380 and num < 425:
+  print("Violet")
